0066bd8db40c794f76859c17582ad391e75072e0
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-05-10 05:20:11 +0900

    리팩토링: 메시지 작성 페이지의 상태 관리 및 폼 로직을 useMessageForm 훅으로 분리

--- hooks/pages/index.ts
+++ hooks/pages/index.ts
@@ -3,3 +3,5 @@ export { useRecipientDetailPage } from './useRecipientDetailPage';
 export { useRecipientsListPage } from './useRecipientsListPage';
 
 export { useNewRecipientForm } from './useNewRecipientForm';
+
+export { useMessageForm } from './useMessageForm';
